38e765355abc853bd8d7d6689d567a2a714c0cd2
Nicolás Miranda <darknamor@gmail.com>
2020-01-25 20:45:05 -0300

    Clase 28 : Callbacks

--- Clases/clase28.js
+++ Clases/clase28.js
@@ -1,8 +1,15 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-
-const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+var signo ;
+var personaje=false;
+do {
+  signo = prompt('Ingresa un numero del 1 al 10')
+  if(parseInt(signo)<=10){
+    personaje=true;
+  }
+} while (personaje==false);
+const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id', signo)}`
 const opts = { crossDomain: true }
 
 const onPeopleResponse = function (persona) {
